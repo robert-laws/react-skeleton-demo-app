3e61f7680854ab00afba1bea0a981835e942a76d
Robert Laws <roblaws@me.com>
2020-05-19 13:27:27 +0300

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'react-skeleton-css/styles/skeleton.2.0.4.css'
+import 'react-skeleton-css/styles/normalize.3.0.2.css';
+import './App.scss';
+
+import placeholderDesktopHD from './img/desktop-hd.jpg'
+import placeholderDesktop from './img/desktop.jpg'
+import placeholderTablet from './img/tablet.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <div className="row">
+          <h1>App Heading</h1>
+        </div>
+        <div className="row">
+          <div className="three columns">App</div>
+          <div className="three columns">App</div>
+          <div className="three columns">App</div>
+          <div className="three columns">App</div>
+        </div>
+        <div className="row">
+          <div className="one columns">space</div>
+          <div className="ten columns">
+            <button className="button-primary">My Button</button>
+          </div>
+          <div className="one columns">space</div>
+        </div>
+      </section>
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <div className="twelve columns">
+              <img className="u-max-full-width" src={placeholderDesktopHD} alt="Placeholder" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="six columns">
+              <img className="u-max-full-width" src={placeholderTablet} alt="Placeholder" />
+            </div>
+            <div className="six columns">
+              <img className="u-max-full-width" src={placeholderTablet} alt="Placeholder" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
